f77d4ac7aa5030d650b900cb24ae55673ded8470
Влад Козлов <vkozlov15012005@gmail.com>
2024-12-27 19:48:04 +0300

    Добавьте файлы проекта.

---
+++ Klinfild/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Klinfild.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Klinfild
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static KlinfildDbEntities _context;
+        public static KlinfildDbEntities GetContext()
+        {
+            if (_context == null)
+            {
+                _context = new KlinfildDbEntities();
+            }
+            return _context;
+        }
+    }
+}
